291ec7855ee748a125459e89292d08ccfe5f8841
Mostafa <mosi3883@gmail.com>
2022-05-07 00:44:20 +0430

    complete

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import SingleColor from './SingleColor';
 import Values from 'values.js';
 
 function App() {
-  const [color, setColor] = useState('#f15025');
-  const [error, setError] = useState('');
-  const [list, setList] = useState([]);
+  const defaultColor = '#f15025';
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values(defaultColor).all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
       setList(colors);
+      setError(false);
     } catch (err) {
       setError(true);
       console.log(err);
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,13 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 const SingleColor = ({ weight, hex, index }) => {
   const [alert, setAlert] = useState(false);
+  const hexValue = `#${hex}`;
+
+  useEffect(() => {
+    let timer;
+    if (alert) {
+      timer = setTimeout(() => {
+        setAlert(false);
+      }, 2500);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
   return (
     <article
       className={`color ${index > 10 ? 'color-light' : ''}`}
-      style={{ backgroundColor: `#${hex}` }}
+      style={{ backgroundColor: hexValue }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hex}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
